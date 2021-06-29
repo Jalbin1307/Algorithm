9ee879cacaca7eb27f8001d90f1461a2c0e01515
Jalbin1307 <az13077@gmail.com>
2021-06-29 17:56:12 +0900

    docs : Add 2. Add Two Numbers.py

---
+++ 2. Add Two Numbers.py
@@ -0,0 +1,22 @@
+ class ListNode(object):
+     def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        root = n = ListNode()
+        carry = 0
+        while l1 or l2 or carry:
+            v1 = v2 = 0
+            if l1:
+                v1 = l1.val
+                l1 = l1.next
+            if l2:
+                v2 = l2.val
+                l2 = l2.next
+            carry, v = divmod(v1+v2+carry,10)
+            n.next = ListNode(v)
+            n = n.next
+            
+        return root.next
